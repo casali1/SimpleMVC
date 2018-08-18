9748b13f400bf28c7ed82714d1a9e5c342e62d08
casali1 <frank_casali@hotmail.com>
2018-08-18 16:54:49 -0600

    Add project files.

---
+++ SimpleMVC/Models/UserModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleMVC.Models
+{
+    public class UserModels
+    {
+        [Display]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Email { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DOB { get; set; }
+        [Range(100, 1000000)]
+        public decimal Salary { get; set; }
+    }
+}
---
+++ SimpleMVC/Models/Users.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleMVC.Models
+{
+    public class Users
+    {
+
+        public List<UserModels> _userList = new List<UserModels>();
+
+        public Users()
+        {
+            _userList.Add(new UserModels
+            {
+                FirstName = "aaa",
+                LastName ="bbb",
+                Address = "XXXXX",
+                Email = "test@test.com",
+                DOB = Convert.ToDateTime("6/22/1976"),
+                Salary = 40000
+            });
+            _userList.Add(new UserModels
+            {
+                FirstName = "ccc",
+                LastName = "ddd",
+                Address = "XXXXX",
+                Email = "test@test.com",
+                DOB = Convert.ToDateTime("6/22/1976"),
+                Salary = 40000
+            });
+
+            _userList.Add(new UserModels
+            {
+                FirstName = "eee",
+                LastName = "fff",
+                Address = "XXXXX",
+                Email = "test@test.com",
+                DOB = Convert.ToDateTime("6/22/1976"),
+                Salary = 40000
+            });
+        }
+
+        public void CreateUser(UserModels userModel)
+        {
+            _userList.Add(userModel);
+        }
+
+        public void UpdateUser(UserModels userModel)
+        {
+            foreach (UserModels user in _userList)
+            {
+                if(user.Email == userModel.Email)
+                {
+                    _userList.Remove(user);
+                    _userList.Add(userModel);
+                    break;
+                }
+            }
+        }
+    }
+}
